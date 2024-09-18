add21d17c9b75dbf0e9cda12f23992578a8cc559
Patricalde <calderonpn1981@gmail.com>
2024-09-18 00:06:06 -0300

    ejercicios

---
+++ javacript/index.js
@@ -0,0 +1,39 @@
+let nombre= "Homero";
+let apellido= "Simpson";
+let edad= 42;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+
+const ciudades1= "Roma";
+const ciudades2= "Florencia";
+const ciudades3= "Venecia";
+const ciudades4= "Milan";
+const ciudades5= "Verona";
+
+console.log(ciudades1 +" "+ ciudades2 + " " + ciudades3 + " "+ciudades4+ " " + ciudades5);
+
+let nombreConductor= "Piero Barone";
+let domicilioConductor="Julio Moreno 2194";
+let paisConductor= "Italia";
+const numeroRegistro= 24597856;
+
+let carnet= (nombreConductor+ " "+domicilioConductor+" "+ paisConductor+ " "+ numeroRegistro);
+ console.log(carnet);
+
+let familiar1= prompt("ingresa el nombre del 1er familiar");
+let familiar2= prompt("ingresa el nombre del 2do familiar");
+let familiar3= prompt("ingresa el nombre del 3er familiar");
+let familiar4= prompt("ingresa el nombre del 4to familiar");
+let familiar5= prompt("ingresa el nombre del 5to familiar");
+
+let flia= "primero integrante"+ familiar1+ "  " + 
+          "segundo integrante" + familiar2+ "  "+
+          "tercer integrante" + familiar3+ "  "+
+          "cuarto integrante" + familiar4+ "  "+
+          "quinto integrante" + familiar5;
+
+console.log (flia);
+
